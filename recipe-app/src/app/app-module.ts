48df67a98358868b578cc14c91665d02734c9920
kimah72 <min23005@byui.edu>
2025-10-01 17:51:26 -0600

    recipe app

---
+++ recipe-app/src/app/app-module.ts
@@ -0,0 +1,30 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { App } from './app';
+import { Header } from './header/header';
+import { Recipes } from './recipes/recipes';
+import { RecipeList } from './recipes/recipe-list/recipe-list';
+import { RecipeDetail } from './recipes/recipe-detail/recipe-detail';
+import { ShoppingList } from './shopping-list/shopping-list';
+import { ShoppingEdit } from './shopping-list/shopping-edit/shopping-edit';
+
+@NgModule({
+  declarations: [
+    App,
+    Header,
+    Recipes,
+    RecipeList,
+    RecipeDetail,
+    ShoppingList,
+    ShoppingEdit
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
