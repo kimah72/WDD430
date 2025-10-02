84701952225c2c595115b9670c0cb376d57345d6
kimah72 <min23005@byui.edu>
2025-10-02 16:34:41 -0600

    files left behind

---
+++ cms/src/app/header.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cms-header',
+  standalone: false,
+  templateUrl: './header.html',
+  styleUrl: './header.css'
+})
+
+export class Header {
+
+}
